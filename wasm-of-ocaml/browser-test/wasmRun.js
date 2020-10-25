f489aaa1c140e90beb1b30f907c2ef3d8b4a6560
Douglas Boyle <dab80@cam.ac.uk>
2020-10-25 11:19:12 +0000

    Moved to using Node.js server to allow fs access

--- wasm-of-ocaml/browser-test/wasmRun.js
+++ wasm-of-ocaml/browser-test/wasmRun.js
@@ -1,16 +1,19 @@
+const fetch = require("node-fetch");
+
 const memory = new WebAssembly.Memory({ initial: 1 });
 
-const log = (offset, length) => {
-  const bytes = new Uint8Array(memory.buffer, offset, length);
-  const string = new TextDecoder('utf8').decode(bytes);
+const util = require('util');
+const fs = require("fs");
 
-  console.log(string);
-};
+const readFile = util.promisify(fs.readFile);
 
 (async () => {
-  const response = await fetch('../C-examples/fib.wasm');
-  const bytes = await response.arrayBuffer();
-  const { instance } = await WebAssembly.instantiate(bytes); //, {
+	const buffer = await readFile('D:/Programming/Project/Wasm-of-ocaml/wasm-of-ocaml/C-examples/fib.wasm');
+  const module = await WebAssembly.compile(buffer);
+  const instance = await WebAssembly.instantiate(module);
+ // const response = await fetch('http://localhost:3000/wasm-files/fib.wasm');
+ // const bytes = await response.arrayBuffer();
+ // const { instance } = await WebAssembly.instantiate(bytes); //, {
   //  env: { log, memory }
 //  });
 
