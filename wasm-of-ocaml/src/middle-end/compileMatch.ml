dbf4aaef1a310a373efd4e465064b8cfa011f72f
Douglas Boyle <dab80@cam.ac.uk>
2020-11-16 13:18:41 +0000

    #8 Get bindings from record patterns

--- wasm-of-ocaml/src/middle-end/compileMatch.ml
+++ wasm-of-ocaml/src/middle-end/compileMatch.ml
@@ -1,11 +1,14 @@
 open LinastUtils
 open Typedtree
+open Types
 
 (* pattern -> comp_expr -> BLet list *)
 (* value of expression needed in case that it first needs binding to something e.g. TPat_var or alias *)
 (* TODO: Must be able to indicate when match has failed e.g. let 5 = x in ...  Need to use fail -1 *)
 (* For 'let pattern = expr ...' statements *)
 (* TODO: Need to track when to use fail -1 (trap) vs fail 0 - depends if inside an OR pattern or not! *)
+(* Inefficient in some places when expr = Compound.Imm (Imm.id x) - creates another identifier bound to the same thing.
+   Should be able to remove this when doing common subexpression elimination/dead assignment elimination. *)
 let rec getBindings (pat : pattern) expr = match pat.pat_desc with
   | Tpat_any -> []
   | Tpat_var (x, _) -> [BLet(x, expr)]
@@ -60,10 +63,22 @@ let rec getBindings (pat : pattern) expr = match pat.pat_desc with
       (LinastExpr.compound (Compound.imm (Imm.fail (-1l))))); (* Test failed - trap TODO: Fail to correct level *)
     ]  @ sub_binds
 
-  | Tpat_record (l, _) -> raise NotImplemented
-  (* Don't expect to need to match Tpat_value or Tpat_lazy -- GADT, they aren't value patterns *)
+  | Tpat_record (l, _) ->
+    let id = Ident.create_local "record" in
+    let record_imm = Imm.id id in
+    let field_binds index (_, labelDesc, pat) =
+      let field_id = Ident.create_local "field" in
+      let field_expr = Compound.imm (Imm.id field_id) in
+      let sub_binds = getBindings pat field_expr in
+      (* Only include the binding for the tuple element if it is actually needed to perform other bindings *)
+      (match sub_binds with [] -> [] | _ -> (BLet(field_id, Compound.field record_imm (Int32.of_int labelDesc.lbl_pos)))::sub_binds)
+    in let binds = List.concat (List.mapi field_binds l) in
+    (match binds with [] -> [] | _ -> (BLet(id, expr)::binds))
+
   (* Unsure about OR case - how does it interact with matching single or multiple components? *)
   | Tpat_or (p1, p2, _) -> raise NotImplemented
+
+  (* Don't expect to need to match Tpat_value or Tpat_lazy -- GADT, they aren't value patterns *)
   | Tpat_array pl -> raise NotImplemented
   | Tpat_variant (_, _, _) -> raise NotImplemented
   | _ -> raise NotSupported
