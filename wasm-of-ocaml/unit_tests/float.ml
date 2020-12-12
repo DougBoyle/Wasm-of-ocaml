2900b7341ee1935a2841d4ca528d0b24ee051600
Douglas Boyle <dab80@cam.ac.uk>
2020-12-12 17:01:22 +0000

    #19 Update tests. Old pattern matching already works for floats

--- wasm-of-ocaml/unit_tests/float.ml
+++ wasm-of-ocaml/unit_tests/float.ml
@@ -1,7 +1,6 @@
 let x = 1.0
 let y = x = 1.0
 let z = x = 1.1
-(*
-let y = x +. 2.0
-let z = match x with 1.0 -> 1 | _ -> 0
-*)
+(* A bad idea to exactly match floats in programs, but as a test program this ensures implementation correct *)
+let w = match x with 1.0 -> 1 | _ -> 0
+let v = match x with 1.1 -> 1 | _ -> 0
