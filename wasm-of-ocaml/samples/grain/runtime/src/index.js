eac6508548c1857f04b6543cfa8652e1092719e7
Douglas Boyle <dab80@cam.ac.uk>
2020-12-12 21:21:08 +0000

    Remove unused directories

--- wasm-of-ocaml/samples/grain/runtime/src/index.js
+++
@@ -1,5 +0,0 @@
-import GrainRunner, { buildGrainRunner } from './runtime';
-import { defaultURLLocator } from './utils/locators';
-import { grainToString } from './utils/utils';
-
-export { GrainRunner, buildGrainRunner, defaultURLLocator, grainToString };
