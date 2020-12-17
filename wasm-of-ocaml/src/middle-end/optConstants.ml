2212c4d3b9e5a89de255d59d5e7ea30798494b16
Douglas Boyle <dab80@cam.ac.uk>
2020-12-17 22:36:43 +0000

    #21 Optimise unary/binary as part of optconstants

--- wasm-of-ocaml/src/middle-end/optConstants.ml
+++ wasm-of-ocaml/src/middle-end/optConstants.ml
@@ -3,9 +3,11 @@
    If x was only being used to assign y, will now be removed as a dead assignment
    TODO: Constant folding i.e. simplifying unops/binops etc. Complex analysis could optimise GetTag too. *)
 open Linast
+open Asttypes
 
 let constant_idents = (Ident.Tbl.create 50 : Asttypes.constant Ident.Tbl.t)
 
+(* Ideally want this to happen after body evaluated, just means extra passes needed *)
 let enter_linast linast = (match linast.desc with
   | LLet(id, _, body, rest) -> (match body.desc with
     (* Only immediate constants, binop(im1, im2) first reduced by constant folding *)
@@ -14,10 +16,70 @@ let enter_linast linast = (match linast.desc with
   | _ -> () (* LLetRec is always a function so no constants to optimise *)
   ); linast
 
-let map_imm (imm : Linast.imm_expr) = match imm.desc with
+let map_imm (imm : imm_expr) = match imm.desc with
   | ImmIdent id ->
     (match Ident.Tbl.find_opt constant_idents id with Some c -> {imm with desc=ImmConst c} | None -> imm)
   | _ -> imm
+
+(* TODO: Update eval_unary/binary if other types ever need supporting for the same ops *)
+let eval_unary = function
+  | (Not, Const_int i) -> Const_int (if i > 0 then 0 else 1) (* 0=false, 1=true *)
+  (* integer *)
+  | (UnNeg, Const_int i) -> Const_int (-i)
+  | (UnAdd, c) -> c (* Could actually do this optimisation any time its seen. Doesn't do anything *)
+  | (Succ, Const_int i) -> Const_int (succ i)
+  | (Pred, Const_int i) -> Const_int (pred i)
+  | (Abs, Const_int i) -> Const_int (abs i)
+  (* float *)
+  | (FUnNeg, Const_float fstr) -> Const_float(Float.to_string(-. (Float.of_string fstr)))
+  | (FSqrt, Const_float fstr) -> Const_float(Float.to_string(sqrt (Float.of_string fstr)))
+  | _ -> failwith "Type mismatch between constant and operator"
+
+let eval_binary = function
+  (* The comparison operations can also take floats.
+     Type checking should have ensured we never see 1 int 1 float, so comparisons defined for whole constant *)
+  | (Eq, c1, c2) -> Const_int (if c1 = c2 then 1 else 0)
+  | (Neq, c1, c2) -> Const_int (if c1 <> c2 then 1 else 0)
+  | (LT, c1, c2) -> Const_int (if c1 < c2 then 1 else 0)
+  | (GT, c1, c2) -> Const_int (if c1 > c2 then 1 else 0)
+  | (LTE, c1, c2) -> Const_int (if c1 <= c2 then 1 else 0)
+  | (GTE, c1, c2) -> Const_int (if c1 >= c2 then 1 else 0)
+  | (Compare, c1, c2) -> Const_int (compare c1 c2)
+
+  | (Min, c1, c2) -> min c1 c2
+  | (Max, c1, c2) -> max c1 c2
+  | (Eq_phys, c1, c2) -> Const_int (if c1 == c2 then 1 else 0)
+  | (Neq_phys, c1, c2) -> Const_int (if c1 != c2 then 1 else 0)
+  (* boolean *)
+  | (AND, Const_int i, Const_int j) -> Const_int (if i > 0 && j > 0 then 1 else 0)
+  | (OR, Const_int i, Const_int j) -> Const_int (if i > 0 || j > 0 then 1 else 0)
+  (* integer *)
+  | (Add, Const_int i, Const_int j) -> Const_int (i + j)
+  | (Sub, Const_int i, Const_int j) -> Const_int (i - j)
+  | (Mult, Const_int i, Const_int j) -> Const_int (i * j)
+  (* Division by zero error is caught by caller and optimisation aborted *)
+  | (Div, Const_int i, Const_int j) -> Const_int (i / j)
+  | (Mod, Const_int i, Const_int j) -> Const_int (i mod j)
+  (* list *)
+  | (Append, _, _) -> failwith "Append should never get constants as arguments" (* Update if tag encoding changes *)
+  (* float *)
+  | (FAdd, Const_float fstr1, Const_float fstr2) ->
+    Const_float(Float.to_string((Float.of_string fstr1) +. (Float.of_string fstr2)))
+  | (FSub, Const_float fstr1, Const_float fstr2) ->
+    Const_float(Float.to_string((Float.of_string fstr1) -. (Float.of_string fstr2)))
+  | (FMult, Const_float fstr1, Const_float fstr2) ->
+    Const_float(Float.to_string((Float.of_string fstr1) *. (Float.of_string fstr2)))
+  | (FDiv, Const_float fstr1, Const_float fstr2) ->
+    Const_float(Float.to_string((Float.of_string fstr1) /. (Float.of_string fstr2)))
+  | _ -> failwith "Type mismatch between constant and operator"
+
+(* Once had the chance to replace immIdents with constants, see if unary/binary can be optimised too *)
+let leave_compound (compound : compound_expr) = match compound.desc with
+  | CUnary(op, ({desc=ImmConst c} as imm)) -> {compound with desc=CImm {imm with desc=ImmConst (eval_unary (op, c))}}
+  | CBinary(op, ({desc=ImmConst c1} as imm1), ({desc=ImmConst c2})) ->
+    (try {compound with desc=CImm {imm1 with desc=ImmConst (eval_binary (op, c1, c2))}}
+     with Division_by_zero -> compound) (* Leave runtime division-by-zero error till runtime *)
+  | _ -> compound
 
 (* Not necessary as ident won't be reused once out of scope, but helps keep table small
    TODO: By doing this, can't optimise bindings passed through an exit to a handler
@@ -32,4 +94,4 @@ let leave_linast linast = (match linast.desc with
 
 let optimise linast =
   Ident.Tbl.clear constant_idents;
-  (LinastMap.create_mapper ~map_imm ~enter_linast ~leave_linast ()) linast
+  (LinastMap.create_mapper ~map_imm ~leave_compound ~enter_linast ~leave_linast ()) linast
---
+++ wasm-of-ocaml/unit_tests/divByZero.ml
@@ -0,0 +1,2 @@
+(* Check that optimisations don't turn a runtime error into a compile time one *)
+let x = 0 / 0
