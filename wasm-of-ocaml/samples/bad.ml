c54321b56cdbe572c5e9b9e269d42028e4655bcb
Douglas Boyle <dab80@cam.ac.uk>
2021-03-06 23:06:51 +0000

    #25 Renaming directories

--- wasm-of-ocaml/samples/bad.ml
+++
@@ -1,3 +0,0 @@
-let rec f x = if x = 0 then 1 else x * (g (x-1))
-
-let a = f 5
--- wasm-of-ocaml/samples/trees.ml
+++ wasm-of-ocaml/samples/trees.ml
@@ -1,13 +1,27 @@
-type 'a tree = Leaf | Node of 'a * 'a tree * 'a tree
+(* Modified version of alltrees from benchmarks to pick up on mark/sweep garbage collection bugs *)
+type tree = Leaf | Node of tree * tree
 
-let rec completeBinaryTree x = function
-  | 0 -> Leaf
-  | n -> Node(x, 
-              completeBinaryTree x (n-1), 
-			  completeBinaryTree x (n-1))
-			  
-let rec countNodes = function
-  | Leaf -> 1
-  | Node(_, l, r) -> 1 + (countNodes l) + (countNodes r)
+(* If map removed, instead get a memory access error (suggesting something was wrongly overwritten) *)
+let rec map f = function
+  | [] -> []
+  | x::xs -> (f x)::(map f xs)
 
-let x = countNodes (completeBinaryTree 0 3)
+(* Error happens even if append written manually, so that isn't the issue
+let rec append l1 l2 = match l1 with
+  | [] -> l2
+  | x::xs -> x::(append xs l2)
+*)
+
+let rec all_trees = function
+  | 0 -> [Leaf]
+  | n ->
+  let rec trees_i i =
+    let left_side = all_trees i
+    and right_side = all_trees (n-1-i) in
+    left_side @ right_side in
+  let rec getall = function
+    | 0 -> trees_i 0
+    | n -> (trees_i n) @ (getall (n-1)) in
+  getall (n-1)
+
+let _ = all_trees 7 (* Works for 6, not for 7 *)
